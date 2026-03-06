72753a7ca30a4573d8a023ec53859fd700583c02
Abdul-Hadi-Jalil <abdulhadijalil34@gmail.com>
2026-03-07 03:03:36 +0500

    added the app icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-//import 'package:pdf_viewer/screens/home_screen.dart';
 import 'package:pdf_viewer/screens/select_pdf_from_local_storage.dart';
-import 'package:pdf_viewer/screens/show_pdf_from_url.dart';
+//import 'package:pdf_viewer/screens/home_screen.dart';
+//import 'package:pdf_viewer/screens/select_pdf_from_local_storage.dart';
+//import 'package:pdf_viewer/screens/show_pdf_from_url.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +14,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'PDF Viewer', home: ShowPdfFromUrl());
+    return MaterialApp(title: 'PDF Viewer', home: ViewPdfFromLocalStorage());
   }
 }
