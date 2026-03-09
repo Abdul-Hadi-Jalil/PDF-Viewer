6ec212a8d4576755764f744c756d0601a0a4f5c7
Abdul-Hadi-Jalil <abdulhadijalil34@gmail.com>
2026-03-09 20:57:58 +0500

    change the app id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pdf_viewer"
+    namespace = "app.abdul_hadi.pdf_viewer"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.pdf_viewer"
+        applicationId = "app.abdul_hadi.pdf_viewer"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
